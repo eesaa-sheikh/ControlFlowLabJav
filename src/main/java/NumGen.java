660c832f4c4aa6b460363e0b631f831411fb6afa
eesaasheikh <eesaasheikh3@gmail.com>
2022-11-10 20:55:47 +0000

    I added some URLs :)

--- src/main/java/NumGen.java
+++ src/main/java/NumGen.java
@@ -6,39 +6,51 @@ public class NumGen {
         int random = Rand.nextInt(5) + 1;
         System.out.println("Welcome to this new guessing game!");
         System.out.println("Are you ready?");
-        System.out.println("Let us begin!");
-        Scanner myInput = new Scanner(System.in);
-        System.out.println("What's your Number:");
-        int Gnum; //Gnum is an integer
-
-        // Each wrong answere gets a Strike.
-        boolean rightAnswer = false;
-
-        {
-            while (!rightAnswer) {
-                Gnum = myInput.nextInt(); // The object I'm guessing is an integer.
-                if (Gnum == random) { // if guess equals number
-                    System.out.println("That is correct! It is" +random );
-                    System.out.println("Here is your present");
-                    System.out.println("https://www.youtube.com/watch?v=QcyPRFpkYak");
-                    myInput.close();
-                    rightAnswer = true;
-
-                } else if (Gnum < random) { //if guess it too low
-                    System.out.println("That is too low!");
-                    System.out.println("Try again: ");
 
 
+        Scanner choice = new Scanner(System.in);
+        String option = choice.nextLine();
+        if (option.equals("Yes")) {
+            System.out.println("The let us begin");
+            //} else if (option.equals("No")) {
+            //System.out.println("That's unfortunate");
+            // myInput.close()
+            // }
 
-                } else if (Gnum > random) { //if guess is too high
-                    System.out.println("That is too high!");
-                    System.out.println("Try again: ");
+            Scanner myInput = new Scanner(System.in);
+            System.out.println("What's your Number:");
+            int Gnum; //Gnum is an integer
 
+            // Each wrong answere gets a Strike.
+            boolean rightAnswer = false;
+
+            {
+                while (!rightAnswer) {
+                    Gnum = myInput.nextInt(); // The object I'm guessing is an integer.
+                    if (Gnum == random) { // if guess equals number
+                        System.out.println("That is correct! It is" + random);
+                        System.out.println("Here is your present");
+                        System.out.println("https://www.youtube.com/watch?v=QcyPRFpkYak");
+                        myInput.close();
+                        rightAnswer = true;
+
+                    } else if (Gnum < random) { //if guess it too low
+                        System.out.println("That is too low!");
+                        System.out.println("Try again: ");
+
+
+                    } else if (Gnum > random) { //if guess is too high
+                        System.out.println("That is too high!");
+                        System.out.println("Try again: ");
+                    }
                 }
-
             }
-
         }
-
+        while (option.equals("No")) {
+            System.out.println("That's unfortunate");
+            System.out.println("Here is your pity prize :(");
+            System.out.println("https://www.youtube.com/watch?v=llqWTJGUFeE ");
+            break;
+            }
     }
 }

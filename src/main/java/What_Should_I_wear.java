40932d6e5307e43325a2c5794055bbafb442334a
Eesaa Sheikh <114883284+eesaa-sheikh@users.noreply.github.com>
2022-11-24 10:13:55 +0000

    Quick Update

--- src/main/java/What_Should_I_wear.java
+++ src/main/java/What_Should_I_wear.java
@@ -1,30 +1,43 @@
 import java.util.Scanner;
-public class What_Should_I_wear {
+public class newWeather {
     public static void main(String[] args) {
-        System.out.println("Let Jarvis pick out your clothes today");
-        System.out.println("what is the weather like today?");
-        Scanner Cweather = new Scanner(System.in);
-        String weather = Cweather.nextLine();
+        int temperature = 20;
 
-        if (weather.equals("bad"));{
-        System.out.println("Please tell me how bad");}
+        System.out.println("Hello, I will be choosing your clothes today!");
 
-        Scanner deg = new Scanner(System.in);
-        String howBad = deg.nextLine();
+        System.out.println("Please tell me what the weather is like");
 
-        if (howBad.equals("Snow"));{
-        System.out.println("That's a problem");
-        System.out.println("Now tell me the temperature?");}
+        //string scanner
+        Scanner op = new Scanner(System.in);
+        String option = op.nextLine();
 
-        Scanner val = new Scanner(System.in);
-        int Temp = val.nextInt();
+        //int Scanner
 
-        if (Temp == 0 );
-        System.out.println("That's brutal!");
-        System.out.println("Here is what I found:" );
-        System.out.println();
-        System.out.println("https://www.shein.co.uk/Men-Double-Breasted-Drop-Shoulder-Belted-Overcoat-p-11457216-cat-3137.html?src_identifier=ai%3D17212%60an%3Duksale%60on%3DBanner%60cn%3D2clearance0725%60hz%3Dclearance0516-01%60ps%3D5%60jc%3DitemPicking_02595039&src_module=campaign&src_tab_page_id=page_activity_factory1668112201180&mallCode=1&scici=campaign_uksale_17212~~ON_1,CN_2clearance0725,HZ_clearance0516-01,HI_hotZone_qijj4ukdt~~1~~itemPicking_02595039~~");
+        int temp = op.nextInt();
 
+        if ((option.equals("Hot") && (temperature < temp))) {
+            System.out.println("Thanks for this input");
+            System.out.println("The temp is " + temp);
+            System.out.println("The whether is " + option);
+            System.out.println("This is hot");
+            System.out.println("This is What I found:https://www.boohooman.com/oversized-graffiti-t-shirt-and-short-set/BMM10414-105-34.html?istCompanyId=9de2f27a-9672-4ac4-b061-147ce3c3a633&istFeedId=9179cc73-11c3-442f-bb56-f89a61fdc694&istItemId=pmplpaaxm&istBid=t&gclid=Cj0KCQiAyMKbBhD1ARIsANs7rEHBmsvt-CMvg3vh9pHxwE2ggB42Qdx1I6B7QEmsPpRdpGpGtMqfa20aAlrjEALw_wcB&gclsrc=aw.ds" );
         }
+        else if (option.equals("Cold") && (temperature > temp)){
+            System.out.println("Thanks for this input");
+            System.out.println("The temp is " + temp);
+            System.out.println("The whether is " + option);
+            System.out.println("This is cold");
+            System.out.println("This is What I found:https://uk.tommy.com/alaska-casual-fit-hooded-puffer-jacket-dm0dm15445bds?utm_source=google&utm_medium=organic&utm_campaign=uk_free-cpc-shopping&cmpid=ch:cpc|so:google|ca:|cr:|lb:18327623425|ag:|pi:&gclid=Cj0KCQiAyMKbBhD1ARIsANs7rEEI0AEkUWi-vSV6FWON-dXgm1BOpeZVR7pLnbpM5uUPbXVPrxyG4kkaAh38EALw_wcB&gclsrc=aw.ds");
+        }
+        System.out.println("Did you enjoy your experience?");
+        Scanner choice = new Scanner(System.in);
+        String chc = choice.nextLine();
+
+        if (chc.equals("Yes")){
+            System.out.println("Thanks for using our service");
+        } 
+            
+        }
+
 
     }
